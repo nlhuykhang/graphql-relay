61fa7dc6bba16382b31a73b7d13c9f1205721369
Khang Nguyen-Le <nlhuykhang@gmail.com>
2016-11-01 19:05:49 +0700

    Remove unused code

--- index.js
+++ index.js
@@ -11,29 +11,13 @@ import assert from 'assert';
 
 const MONGO_URL = 'mongodb://localhost:27017/test';
 
-// import readline from 'readline';
-
 import mySchema from './schema/main.js';
 
 const app = express();
 
-// const rli = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-
 mgClient.connect(MONGO_URL, (err, db) => {
   assert.equal(null, err);
   console.log('Connected to MongoDB server');
-
-  // rli.question('Client request:', inputQuery => {
-  //   ql(mySchema, inputQuery, {}, { db }).then(result => {
-  //     console.log('Server answer: ', result.data);
-  //     db.close(() => rli.close());
-  //   });
-  //
-  //   rli.close();
-  // });
 
   app.use('/graphql', qlHttp({
     schema: mySchema,
